e6d42dfa236398504e24bc4a00c45f7d7df535a9
bbffbb <birkirfb13@ru.is>
2018-02-10 15:36:05 +0000

    formspree email service added

--- src/pages/index.js
+++ src/pages/index.js
@@ -116,22 +116,26 @@ class HomeIndex extends React.Component {
                         </ul>
                     </section>
                     */}
-                    
+                 
                     <section id="three">
                             <h2>Get In Touch</h2>
                             <p>If you think you've got a job for me or have something awesome to tell me please send me a message and i'll reply as soon as possible.</p>
                             <div className="row">
                                 <div className="8u 12u$(small)">
-                                    <form method="post" action="#" data-netlify="true">
+                                    <form method="POST" action="https://formspree.io/birkirfreyrbaldurss@gmail.com">
                                         <div className="row uniform 50%">
                                             <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                            <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                            <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+                                            <div className="6u 12u$(xsmall)">
+                                                <input type="email" name="email" id="email" placeholder="Your email"></input>
+                                            </div>
+                                            <div className="12u">
+                                                <textarea name="message" id="message" placeholder="Your message" rows="4"></textarea>
+                                            </div>
                                         </div>
+                                        <ul className="actions">
+                                            <li><input type="submit" value="Send" /></li>
+                                        </ul>   
                                     </form>
-                                    <ul className="actions">
-                                        <li><input type="submit" value="Send Message" /></li>
-                                    </ul>
                                 </div>
                                 <div className="4u 12u$(small)">
                                     <ul className="labeled-icons">
