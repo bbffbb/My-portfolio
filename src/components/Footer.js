e911b43af295f226e82dc75184bd0879ceb24b98
bbffbb <birkirfb13@ru.is>
2018-02-14 23:21:35 +0000

    social links open in new tabs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="https://www.facebook.com/birkirfreyr.baldursson" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-                        <li><a href="https://www.instagram.com/bibbster10/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-                        <li><a href="https://www.linkedin.com/in/birkir-freyr-baldursson" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-                        <li><a href="https://open.spotify.com/user/birkirfreyrbaldurss/playlist/5iTLBpDdOo0XP2pOgUFFga" className="icon fa-spotify"><span className="label">Spotify</span></a></li>
-                        <li><a href="https://twitter.com/birkirfr" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="https://github.com/bbffbb" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="https://www.facebook.com/birkirfreyr.baldursson" className="icon fa-facebook" target="_blank"><span className="label">Facebook</span></a></li>
+                        <li><a href="https://www.instagram.com/bibbster10/" className="icon fa-instagram" target="_blank"><span className="label">Instagram</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/birkir-freyr-baldursson" className="icon fa-linkedin" target="_blank"><span className="label">LinkedIn</span></a></li>
+                        <li><a href="https://open.spotify.com/user/birkirfreyrbaldurss/playlist/5iTLBpDdOo0XP2pOgUFFga" className="icon fa-spotify" target="_blank"><span className="label">Spotify</span></a></li>
+                        <li><a href="https://twitter.com/birkirfr" className="icon fa-twitter"><span className="label" target="_blank">Twitter</span></a></li>
+                        <li><a href="https://github.com/bbffbb" className="icon fa-github"><span className="label" target="_blank">Github</span></a></li>
                     </ul>
                 </div>
             </div>
