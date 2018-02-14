54c34bb500f13e420f8406859df25ba5f019126a
bbffbb <birkirfb13@ru.is>
2018-02-14 11:55:38 +0000

    testing link tracking GA

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="https://www.facebook.com/birkirfreyr.baldursson" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+                        <li><a href="https://www.facebook.com/birkirfreyr.baldursson" onClick="_gaq.push(['_trackEvent', 'LinkClicks', 'click’, '0']);"className="icon fa-facebook"><span className="label">Facebook</span></a></li>
                         <li><a href="https://www.instagram.com/bibbster10/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
                         <li><a href="https://www.linkedin.com/in/birkir-freyr-baldursson" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
                         <li><a href="https://open.spotify.com/user/birkirfreyrbaldurss/playlist/5iTLBpDdOo0XP2pOgUFFga" className="icon fa-spotify"><span className="label">Spotify</span></a></li>
