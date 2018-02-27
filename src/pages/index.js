541b11e93d2d7e374680d6a78f37263d5bf85eb2
bbffbb <birkirfb13@ru.is>
2018-02-27 18:13:47 +0000

    form handling test netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,6 +121,24 @@ class HomeIndex extends React.Component {
                             <p>If you think you've got a job for me or have something awesome to tell me please send me a message and i'll reply as soon as possible.</p>
                             <div className="row">
                                 <div className="8u 12u$(small)">
+                                <form name="contact" method="POST" netlify>
+                                    <div className="row uniform 50%">
+                                            <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
+                                            <div className="6u 12u$(xsmall)">
+                                                <input type="email" name="email" id="email" placeholder="Your email"></input>
+                                            </div>
+                                            <div className="12u">
+                                                <textarea name="message" id="message" placeholder="Your message" rows="4"></textarea>
+                                            </div>
+                                        </div>
+                                        <ul className="actions">
+                                            <li><input type="submit" value="Send" /></li>
+                                            <li>
+                                                <div netlify-recaptcha></div>
+                                            </li>
+                                        </ul>   
+                                </form>
+                                {/*
                                     <form method="POST" action="https://formspree.io/birkirfreyrbaldurss@gmail.com">
                                         <div className="row uniform 50%">
                                             <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
@@ -135,6 +153,7 @@ class HomeIndex extends React.Component {
                                             <li><input type="submit" value="Send" /></li>
                                         </ul>   
                                     </form>
+                                */}
                                 </div>
                                 <div className="4u 12u$(small)">
                                     <ul className="labeled-icons">
